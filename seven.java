70127d316ba2fcecab9c7e89743e33f1834a7e5b
WantingTsai <m840428guy@yahoo.com.tw>
2016-03-04 11:23:28 +0800

    seven.java

---
+++ seven.java
@@ -0,0 +1,9 @@
+class seven{
+	public static void main(String[] args) {
+ 		System.out.println("烏衣巷  劉禹錫");
+		System.out.println("\t朱雀橋邊野草花，");
+		System.out.println("\t烏衣巷口夕陽斜。");
+		System.out.println("\t舊時王謝堂前燕，");
+		System.out.println("\t飛入尋常百姓家。");
+	}
+}
